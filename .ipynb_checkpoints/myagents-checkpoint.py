f882ad524cf17fa1d88adf5ed27e538977be3a07
Obed Otto C <obedotto@gmail.com>
2022-03-31 13:54:09 +0530

    Created

---
+++ .ipynb_checkpoints/myagents-checkpoint.py
@@ -0,0 +1,189 @@
+import random
+
+class Thing:
+    """
+        This represents any physical object that can appear in an Environment.
+    """
+
+    def is_alive(self):
+        """Things that are 'alive' should return true."""
+        return hasattr(self, 'alive') and self.alive
+
+    def show_state(self):
+        """Display the agent's internal state. Subclasses should override."""
+        print("I don't know how to show_state.")
+
+
+class Agent(Thing):
+    """
+        An Agent is a subclass of Thing
+    """
+
+    def __init__(self, program=None):
+        self.alive = True
+        self.performance = 0
+        self.program = program
+
+    def can_grab(self, thing):
+        """Return True if this agent can grab this thing.
+        Override for appropriate subclasses of Agent and Thing."""
+        return False
+
+def TableDrivenAgentProgram(table):
+    """
+    [Figure 2.7]
+    This agent selects an action based on the percept sequence.
+    It is practical only for tiny domains.
+    To customize it, provide as table a dictionary of all
+    {percept_sequence:action} pairs.
+    """
+    percepts = []
+
+    def program(percept):
+        action =None
+        """
+        Write your code here
+        """
+        return action
+
+    return program
+
+loc_A, loc_B = (0, 0), (1, 0)  # The two locations for the Vacuum world
+
+
+def TableDrivenVacuumAgent():
+    """
+    Tabular approach towards vacuum world
+    """
+    table = {((loc_A, 'Clean'),): 'Right',
+             ((loc_A, 'Dirty'),): 'Suck',
+             ((loc_B, 'Clean'),): 'Left',
+             ((loc_B, 'Dirty'),): 'Suck',
+             ((loc_A, 'Dirty'), (loc_A, 'Clean')): 'Right',
+             ((loc_A, 'Clean'), (loc_B, 'Dirty')): 'Suck',
+             ((loc_B, 'Clean'), (loc_A, 'Dirty')): 'Suck',
+             ((loc_B, 'Dirty'), (loc_B, 'Clean')): 'Left',
+             ((loc_A, 'Dirty'), (loc_A, 'Clean'), (loc_B, 'Dirty')): 'Suck',
+             ((loc_B, 'Dirty'), (loc_B, 'Clean'), (loc_A, 'Dirty')): 'Suck'}
+    return Agent(TableDrivenAgentProgram(table))
+
+
+class Environment:
+    """Abstract class representing an Environment. 'Real' Environment classes
+    inherit from this. Your Environment will typically need to implement:
+        percept:           Define the percept that an agent sees.
+        execute_action:    Define the effects of executing an action.
+                           Also update the agent.performance slot.
+    The environment keeps a list of .things and .agents (which is a subset
+    of .things). Each agent has a .performance slot, initialized to 0.
+    Each thing has a .location slot, even though some environments may not
+    need this."""
+
+    def __init__(self):
+        self.things = []
+        self.agents = []
+
+    def percept(self, agent):
+        """Return the percept that the agent sees at this point. (Implement this.)"""
+        raise NotImplementedError
+
+    def execute_action(self, agent, action):
+        """Change the world to reflect this action. (Implement this.)"""
+        raise NotImplementedError
+
+    def default_location(self, thing):
+        """Default location to place a new thing with unspecified location."""
+        return None
+
+    def is_done(self):
+        """By default, we're done when we can't find a live agent."""
+        return not any(agent.is_alive() for agent in self.agents)
+
+    def step(self):
+        """Run the environment for one time step. If the
+        actions and exogenous changes are independent, this method will
+        do. If there are interactions between them, you'll need to
+        override this method."""
+        if not self.is_done():
+            actions = []
+            for agent in self.agents:
+                if agent.alive:
+                    actions.append(agent.program(self.percept(agent)))
+                else:
+                    actions.append("")
+            for (agent, action) in zip(self.agents, actions):
+                self.execute_action(agent, action)
+
+    def run(self, steps=1000):
+        """Run the Environment for given number of time steps."""
+        for step in range(steps):
+            if self.is_done():
+                return
+            self.step()
+
+    def add_thing(self, thing, location=None):
+        """Add a thing to the environment, setting its location. For
+        convenience, if thing is an agent program we make a new agent
+        for it. (Shouldn't need to override this.)"""
+        if not isinstance(thing, Thing):
+            thing = Agent(thing)
+        if thing in self.things:
+            print("Can't add the same thing twice")
+        else:
+            thing.location = location if location is not None else self.default_location(thing)
+            self.things.append(thing)
+            if isinstance(thing, Agent):
+                thing.performance = 0
+                self.agents.append(thing)
+
+    def delete_thing(self, thing):
+        """Remove a thing from the environment."""
+        try:
+            self.things.remove(thing)
+        except ValueError as e:
+            print(e)
+            print("  in Environment delete_thing")
+            print("  Thing to be removed: {} at {}".format(thing, thing.location))
+            print("  from list: {}".format([(thing, thing.location) for thing in self.things]))
+        if thing in self.agents:
+            self.agents.remove(thing)
+
+
+class TrivialVacuumEnvironment(Environment):
+    """This environment has two locations, A and B. Each can be Dirty
+    or Clean. The agent perceives its location and the location's
+    status. This serves as an example of how to implement a simple
+    Environment."""
+
+    def __init__(self):
+        super().__init__()
+        self.status = {loc_A: random.choice(['Clean', 'Dirty']),
+                       loc_B: random.choice(['Clean', 'Dirty'])}
+
+    def thing_classes(self):
+        return [ TableDrivenVacuumAgent]
+
+    def percept(self, agent):
+        """Returns the agent's location, and the location status (Dirty/Clean)."""
+        return agent.location, self.status[agent.location]
+
+    def execute_action(self, agent, action):
+        """Change agent's location and/or location's status; track performance.
+        Score 10 for each dirt cleaned; -1 for each move."""
+
+        """
+        Write your code here
+        """
+
+    def default_location(self, thing):
+        """Agents start in either location at random."""
+        return random.choice([loc_A, loc_B])
+
+
+if __name__ == "__main__":
+    agent = TableDrivenVacuumAgent()
+    environment = TrivialVacuumEnvironment()
+    environment.add_thing(agent)
+    print(environment.status)
+    environment.run()
+    print(agent.performance)
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# aiagent
-Developing AI Agent with PEAS Description
+# Developing AI Agent with PEAS Description
+
+## AIM
+
+To find the PEAS description for the given AI problem and develop an AI agent.
+
+## THEORY
+Explain the problem statement
+
+## PEAS DESCRIPTION
+
+
+## DESIGN STEPS
+### STEP 1:
+Identifying the input:
+
+### STEP 2:
+Identifying the output:
+
+### STEP 3:
+Developing the PEAS description:
+### STEP 4:
+Implementing the AI agent
+
+### STEP 5:
+Measure the performance parameters
+
+## PROGRAM
+Include your agent code here
+
+## OUTPUT
+
+
+## RESULT
+
